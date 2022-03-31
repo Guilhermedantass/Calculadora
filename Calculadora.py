5def3ab6931b44c9a7fe85724b5cfe075642501a
Guilherme <elisongdm1911@gmail.com>
2022-03-30 23:05:31 -0300

    Entendi como funciona os botons e ta rodando

--- Calculadora.py
+++ Calculadora.py
@@ -1,5 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.config import Config
+
+Config.set('graphics', 'width', '320')
+Config.set('graphics', 'height', '500')
 
 GUI = Builder.load_file('tela.kv')
 
@@ -7,6 +11,12 @@ GUI = Builder.load_file('tela.kv')
 class MinhaCalculadora(App):
     def build(self):
         return GUI
+
+    def teste(self):
+        self.root.ids['operacao'].text += 'X'
+
+    def on_start(self):
+        self.root.ids['x'].on_press = self.teste
 
 
 MinhaCalculadora().run()
