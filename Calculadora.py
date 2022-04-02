c40fe05726b8266dc996bf94ffd6cdcaf6d63a18
Guilherme <elisongdm1911@gmail.com>
2022-04-01 22:39:36 -0300

    add C e CE na calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -5,6 +5,8 @@ from kivy.config import Config
 Config.set('graphics', 'width', '320')
 Config.set('graphics', 'height', '500')
 
+from kivy.core.window import Window
+
 GUI = Builder.load_file('tela.kv')
 
 
@@ -13,6 +15,8 @@ GUI = Builder.load_file('tela.kv')
 class MinhaCalculadora(App):
     def build(self):
         return GUI
+
+
     OPERECAO = ''
 
     def soma(self):
@@ -64,15 +68,42 @@ class MinhaCalculadora(App):
         else:
             pass
 
+    def ce(self):
+        GUI.ids['input'].text = ''
 
+    def c(self):
+        MinhaCalculadora.NUMBER_ONE = ''
+        GUI.ids['input'].text = ''
+        GUI.ids['operacao'].text = ''
+        MinhaCalculadora.OPERECAO = ''
 
     GUI.ids['divisao'].bind(on_press=divisao)
     GUI.ids['multiplicacao'].bind(on_press=multiplicacao)
     GUI.ids['subtracao'].bind(on_press=subtracao)
     GUI.ids['soma'].bind(on_press=soma)
     GUI.ids['resultado'].bind(on_press=resultado)
+    GUI.ids['ce'].bind(on_press=ce)
+    GUI.ids['c'].bind(on_press=c)
 
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        print(keycode[1])
+        if keycode[1] == 'numpadadd':
+            self.soma()
+        elif keycode[1] == 'numpadsubstract':
+            self.subtracao()
+        elif keycode[1] == 'numpadmul':
+            self.multiplicacao()
+        elif keycode[1] == 'numpaddivide':
+            self.divisao()
+        elif keycode[1] == 'enter' or keycode[1] == 'numpadenter':
+            self.subtracao()
+        return True
 
 
 if __name__ == '__main__':
     MinhaCalculadora().run()
+
