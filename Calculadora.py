a3fcdb60bad497646ea6b8ebed641650f526bcba
Guilherme <elisongdm1911@gmail.com>
2022-04-02 11:31:36 -0300

    Add teclado

--- Calculadora.py
+++ Calculadora.py
@@ -6,10 +6,9 @@ Config.set('graphics', 'width', '320')
 Config.set('graphics', 'height', '500')
 
 from kivy.core.window import Window
+from kivy.uix.widget import Widget
 
 GUI = Builder.load_file('tela.kv')
-
-
 
 
 class MinhaCalculadora(App):
@@ -17,33 +16,34 @@ class MinhaCalculadora(App):
     OPERECAO = ''
     NUMBER_ONE = ''
     NUMBER_TWO=''
+    resultado=''
 
     def build(self):
         return GUI
 
     def soma(self):
         MinhaCalculadora.NUMBER_ONE = GUI.ids['input'].text
-        GUI.ids['input'].text = ''
         GUI.ids['operacao'].text = f'{MinhaCalculadora.NUMBER_ONE} +'
         MinhaCalculadora.OPERECAO = 'SOMA'
+        GUI.ids['input'].text = ''
 
     def multiplicacao(self):
         MinhaCalculadora.NUMBER_ONE = GUI.ids['input'].text
-        GUI.ids['input'].text = ''
         GUI.ids['operacao'].text = f'{MinhaCalculadora.NUMBER_ONE} *'
         MinhaCalculadora.OPERECAO = 'MULTIPLICAÇÃO'
+        GUI.ids['input'].text = ''
 
     def subtracao(self):
         MinhaCalculadora.NUMBER_ONE = GUI.ids['input'].text
-        GUI.ids['input'].text = ''
         GUI.ids['operacao'].text = f'{MinhaCalculadora.NUMBER_ONE} -'
         MinhaCalculadora.OPERECAO = 'SUBTRAÇÃO'
+        GUI.ids['input'].text = ''
 
     def divisao(self):
         MinhaCalculadora.NUMBER_ONE = GUI.ids['input'].text
-        GUI.ids['input'].text = ''
         GUI.ids['operacao'].text = f'{MinhaCalculadora.NUMBER_ONE} /'
         MinhaCalculadora.OPERECAO = 'DIVISAO'
+        GUI.ids['input'].text = ''
 
     def elevacao_por2(self):
         numero = float(GUI.ids['input'].text)**2
@@ -125,27 +125,9 @@ class MinhaCalculadora(App):
 
     def c(self):
         MinhaCalculadora.NUMBER_ONE = ''
+        MinhaCalculadora.OPERECAO = ''
         GUI.ids['input'].text = ''
         GUI.ids['operacao'].text = ''
-        MinhaCalculadora.OPERECAO = ''
-
-    def _keyboard_closed(self):
-        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
-        self._keyboard = None
-
-    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
-        print(keycode[1])
-        if keycode[1] == 'numpadadd':
-            self.soma()
-        elif keycode[1] == 'numpadsubstract':
-            self.subtracao()
-        elif keycode[1] == 'numpadmul':
-            self.multiplicacao()
-        elif keycode[1] == 'numpaddivide':
-            self.divisao()
-        elif keycode[1] == 'enter' or keycode[1] == 'numpadenter':
-            self.subtracao()
-        return True
 
     GUI.ids['divisao'].bind(on_press=divisao)
     GUI.ids['multiplicacao'].bind(on_press=multiplicacao)
@@ -168,6 +150,63 @@ class MinhaCalculadora(App):
     GUI.ids['0'].bind(on_press=zero)
 
 
+c = MinhaCalculadora()
+
+
+class MyKeyboardListener(Widget):
+
+    def __init__(self, **kwargs):
+        super(MyKeyboardListener, self).__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(
+            self._keyboard_closed, self, 'text')
+        if self._keyboard.widget:
+            # If it exists, this widget is a VKeyboard object which you can use
+            # to change the keyboard layout.
+            pass
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+    def _keyboard_closed(self):
+        print('My keyboard have been closed!')
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        print(keycode[1])
+        if keycode[1] == 'numpadadd':
+            c.soma()
+        elif keycode[1] == 'numpadsubstract':
+            c.subtracao()
+        elif keycode[1] == 'numpadmul':
+            c.multiplicacao()
+        elif keycode[1] == 'numpaddivide':
+            c.divisao()
+        elif keycode[1] == 'enter' or keycode[1] == 'numpadenter':
+            c.subtracao()
+        elif keycode[1] == '1' or keycode[1] == 'numpad1':
+            c.um()
+        elif keycode[1] == '2' or keycode[1] == 'numpad2':
+            c.dois()
+        elif keycode[1] == '3' or keycode[1] == 'numpad3':
+            c.tres()
+        elif keycode[1] == '4' or keycode[1] == 'numpad4':
+            c.quatro()
+        elif keycode[1] == '5' or keycode[1] == 'numpad5':
+            c.cinco()
+        elif keycode[1] == '6' or keycode[1] == 'numpad6':
+            c.seis()
+        elif keycode[1] == '7' or keycode[1] == 'numpad7':
+            c.sete()
+        elif keycode[1] == '8' or keycode[1] == 'numpad8':
+            c.oito()
+        elif keycode[1] == '9' or keycode[1] == 'numpad9':
+            c.nove()
+        elif keycode[1] == '0' or keycode[1] == 'numpad0':
+            c.zero()
+
+        return True
+
+
 if __name__ == '__main__':
-    MinhaCalculadora().run()
+    MyKeyboardListener()
+    c.run()
 
